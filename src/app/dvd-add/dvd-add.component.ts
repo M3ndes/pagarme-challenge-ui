db96c179938d7ed447c33fe7c8f55e82181c3ab0
Thales Mendes <thalesmendes.dev@gmail.com>
2021-10-01 10:44:16 -0300

    added dvd crud and validations

--- src/app/dvd-add/dvd-add.component.ts
+++ src/app/dvd-add/dvd-add.component.ts
@@ -1,37 +1,77 @@
 import { Component, Inject, OnInit } from '@angular/core';
-import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Router } from '@angular/router';
 import { DvdService } from '../services/dvd.service';
+import { ContactService } from '../services/contact.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-dvd-add',
   templateUrl: './dvd-add.component.html',
   styleUrls: ['./dvd-add.component.css'],
-  providers: [DvdService]
+  providers: [DvdService, ContactService]
 })
 export class DvdAddComponent implements OnInit {
+  contacts: any[] = [];
+  slide = false;
+  contact_id = 0;
 
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: any,
     public dialogRef: MatDialogRef<DvdAddComponent>,
     private dvdService: DvdService,
+    private contactService: ContactService,
     private router: Router
   ) { }
 
-  ngOnInit(): void {
+  async ngOnInit(): Promise<void> {
+    const contacts = await this.contactService.all();
+    this.contacts.push(contacts);
+    this.contacts = [...this.contacts];
   }
 
   async saveDvd(data: any): Promise<void> {
-    const payload = new FormData();
-    payload.append('name', data.dvd.name);
-    payload.append('borrowed', 'false');
+    if (!data.dvd.name) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'você precisa informar um nome para o dvd.'
+      });
+      return;
+    }
+    if (this.slide == true && !this.contact_id) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'você precisa informar um contato em caso de empréstimo.'
+      });
+      return;
+    }
+
+    const payload = { name: data.dvd.name, borrowed: this.isChecked(), contact_id: this.contact_id };
     await this.dvdService.create(payload);
     this.dialogRef.close();
     this.reload('/dvd');
   }
 
   async editDvd(data: any): Promise<void> {
-    const body = { name: data.dvd.name, borrowed: 'false', contact_id: '1' };
+    if (!data.dvd.name) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'você precisa informar um nome para o dvd.'
+      });
+      return;
+    }
+    if (this.slide == true && !this.contact_id) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'você precisa informar um contato em caso de empréstimo.'
+      });
+      return;
+    }
+    const body = { name: data.dvd.name, borrowed: this.isChecked(), contact_id: this.contact_id };
     await this.dvdService.update(body, data.dvd.id);
     this.dialogRef.close();
     this.reload('/dvd');
@@ -41,9 +81,23 @@ export class DvdAddComponent implements OnInit {
     this.dialogRef.close();
   }
 
+  slideChange(data: any): void {
+    this.slide = data.checked;
+    if (this.slide == false) {
+      this.contact_id = 0;
+    }
+  }
+
+  isChecked(): boolean {
+    return this.slide;
+  }
+
+  changeContact(value: any): void {
+    this.isChecked() ? this.contact_id = value : this.contact_id = 0;
+  }
+
   async reload(url: string): Promise<boolean> {
     await this.router.navigateByUrl('/', { skipLocationChange: true });
     return this.router.navigateByUrl(url);
   }
-
 }
--- src/app/dvd-list/dvd-list.component.ts
+++ src/app/dvd-list/dvd-list.component.ts
@@ -1,14 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-import { DvdService } from '../services/dvd.service';
 import { MatDialog } from '@angular/material/dialog';
+import { DvdService } from '../services/dvd.service';
 import { DvdAddComponent } from '../dvd-add/dvd-add.component';
 import { Router } from '@angular/router';
+import { ContactService } from '../services/contact.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-dvd-list',
   templateUrl: './dvd-list.component.html',
   styleUrls: ['./dvd-list.component.css'],
-  providers: [DvdService]
+  providers: [DvdService, ContactService]
 })
 export class DvdListComponent implements OnInit {
   name!: string;
@@ -18,18 +20,63 @@ export class DvdListComponent implements OnInit {
   constructor(
     public dialog: MatDialog,
     private dvdService: DvdService,
+    private contactService: ContactService,
     private router: Router
   ) { }
 
   async ngOnInit(): Promise<void> {
     const dvd = await this.dvdService.all();
+    this.getNames(dvd);
     this.dataSource.push(dvd);
     this.dataSource = [...this.dataSource];
   }
 
   async deleteDvd(id: any): Promise<void> {
-    await this.dvdService.delete(id);
-    this.reload('/dvd');
+    const result = await Swal.fire({
+      title: 'Atenção!',
+      text: 'Você tem certeza que deseja deletar esse registro?',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      cancelButtonText: 'Cancelar',
+      confirmButtonText: 'Continuar'
+    });
+
+    if (result.isConfirmed) {
+      await this.dvdService.delete(id);
+      this.reload('/dvd');
+      const Toast = Swal.mixin({
+        toast: true,
+        position: 'top-end',
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+          toast.addEventListener('mouseenter', Swal.stopTimer);
+          toast.addEventListener('mouseleave', Swal.resumeTimer);
+        }
+      });
+      Toast.fire({
+        icon: 'success',
+        title: 'registro deletado com sucesso.'
+      });
+    }
+
+  }
+
+  async getNames(dvd: any): Promise<void> {
+    if (dvd) {
+      for (let i = 0; i < dvd.length; i++) {
+        if (dvd[i].contact_id) {
+          const contact = await this.contactService.find(dvd[i].contact_id);
+          dvd[i].contact_id = contact.name;
+        } else {
+          dvd[i].contact_id = '';
+        }
+      }
+    }
+    return;
   }
 
   dvdAdd(dvd: any): void {
@@ -48,5 +95,5 @@ export class DvdListComponent implements OnInit {
     await this.router.navigateByUrl('/', { skipLocationChange: true });
     return this.router.navigateByUrl(url);
   }
-  
+
 }
